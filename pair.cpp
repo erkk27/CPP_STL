7acf57ff5d8567c6afca2262c4800806fb8828ec
Yukai <2897622096@qq.com>
2025-07-09 14:06:30 +0800

    priority part added

--- pair.cpp
+++ pair.cpp
@@ -16,6 +16,21 @@ int main(){
     auto p3 = make_pair("key",13);
 
     pair<char,bool>p4{'a',true};
-    
+
+    //compare: pair.fist then pair.second
+    pair<int,int> a(1,10);
+    pair<int,int> b(1,6);
+    pair<int,int> c(3,3);
+
+    bool res1 = (a < b); //true 
+    bool res2 = (a < c); //false 
+
+    //use in function
+    pair<int,int> divide(int a,int b);
+    pair<int,int> divide(int a,int b){
+        return {a / b, a % b};
+    }
+
+    std::pair<int, int> result = divide(10, 3);
     return 0;
 }
